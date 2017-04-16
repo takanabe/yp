bc79ad0125cda76ed3e08dca176dec7c11ae4f8e
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2017-04-16 12:56:26 +0900

    Add function to fetch data from Google sreadsheet

---
+++ helper/google_drive_handler.rb
@@ -0,0 +1,18 @@
+require "google_drive"
+
+def find_password_from_google_spreadsheet(session, spreadsheet_id, target_key)
+  ws = session.spreadsheet_by_key(spreadsheet_id).worksheets.first
+  (1..ws.num_rows).each do |row|
+    key = ws[row, 1].strip
+    password = ws[row, 2].strip
+
+    if target_key == key
+      return password
+    end
+  end
+end
+
+## shared test spread sheet id
+#spreadsheet_id = "1S63IDvYxYIVjWg-UodB3ek7oCMGMLLKIm0UIJ_4R3Ho"
+#puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key")
+#puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key2")
---
+++ yp.rb
@@ -0,0 +1,13 @@
+require 'dotenv/load'
+require File.expand_path('helper/google_drive_handler.rb', __dir__)
+
+# Creates a session. This will prompt the credential via command line for the
+# first time and save it to config.json file for later usages.
+# See this document to learn how to create config.json:
+# https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
+session = GoogleDrive::Session.from_config(".config.json")
+
+# shared test spread sheet id
+spreadsheet_id = "1S63IDvYxYIVjWg-UodB3ek7oCMGMLLKIm0UIJ_4R3Ho"
+puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key")
+puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key2")
