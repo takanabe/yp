9a5fe8304c417b59566552e154114aa322798d25
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2017-04-16 19:20:58 +0900

    Add function to fetch github activities

--- yp.rb
+++ yp.rb
@@ -1,5 +1,6 @@
 require 'dotenv/load'
 require File.expand_path('helper/google_drive_handler.rb', __dir__)
+require 'furik/cli'
 
 # Creates a session. This will prompt the credential via command line for the
 # first time and save it to config.json file for later usages.
@@ -8,6 +9,26 @@ require File.expand_path('helper/google_drive_handler.rb', __dir__)
 session = GoogleDrive::Session.from_config(".config.json")
 
 # shared test spread sheet id
-spreadsheet_id = "1S63IDvYxYIVjWg-UodB3ek7oCMGMLLKIm0UIJ_4R3Ho"
-puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key")
-puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key2")
+#spreadsheet_id = "1S63IDvYxYIVjWg-UodB3ek7oCMGMLLKIm0UIJ_4R3Ho"
+#puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key")
+#puts find_password_from_google_spreadsheet(session, spreadsheet_id, "test_key2")
+
+def fetch_github_activity_today
+  system('furik activity')
+end
+
+def fetch_github_activity_yesteday
+  system("furik activity\s" \
+         "--from #{(Date.today - 1).to_s} --to #{(Date.today - 1).to_s}")
+end
+
+def generate_report_for_today
+  fetch_github_activity_today
+end
+
+def generate_report_for_yesterday
+  fetch_github_activity_yesteday
+end
+
+# main
+generate_report_for_today
