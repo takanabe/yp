940be57a0b2adf803798fcdb09196054b1391b2a
Takayuki WATANABE <takayuki-watanabe@cookpad.com>
2017-04-22 17:11:53 +0900

    Change configurations

--- yp.rb
+++ yp.rb
@@ -7,32 +7,54 @@ require 'furik/cli'
 $session = GoogleDrive::Session.from_config(".config.json")
 $spreadsheet_id = ENV["GOOGLE_SPREAD_SHEET_ID"]
 
+
 def fetch_github_activity_today
-  system('furik activity')
+  system('furik activity -l')
 end
 
 def fetch_github_activity_yesteday
-  system("furik activity\s" \
+  system("furik activity -l \s" \
          "--from #{(Date.today - 1).to_s} --to #{(Date.today - 1).to_s}")
 end
 
 def generate_report_for_today
-  fetch_github_activity_today
   fetch_wunderlist_activoty_for_today
+  fetch_github_activity_today
 end
 
 def generate_report_for_yesterday
+  fetch_wunderlist_activoty_for_yesterday
   fetch_github_activity_yesteday
 end
 
 def fetch_wunderlist_activoty_for_today
   wl = Wunderlist.new()
-  wl.fetch_all_lists_from_folder("Cookpad")
-  puts "# Wunderlist activity"
-  wl.fetch_completed_tasks_for_list.each do |completed_task|
-    puts "- #{completed_task}"
+  wl.fetch_all_lists_from_folder(EVN["TARGET_FOLDER"])
+  puts "# Wunderlist activities"
+  wl.fetch_completed_tasks_for_list
+  wl.completed_tasks.each do |list,tasks|
+    puts "## #{list}"
+    tasks.each do |task|
+      puts "- #{task}"
+    end
+    puts ""
+  end
+end
+
+def fetch_wunderlist_activoty_for_yesterday
+  wl = Wunderlist.new()
+  wl.fetch_all_lists_from_folder(EVN["TARGET_FOLDER"])
+  puts "# Wunderlist activities"
+  wl.fetch_completed_tasks_for_list_yesterday
+  wl.completed_tasks.each do |list,tasks|
+    puts "## #{list}"
+    tasks.each do |task|
+      puts "- #{task}"
+    end
+    puts ""
   end
 end
 
 # main
 generate_report_for_today
+# generate_report_for_yesterday
